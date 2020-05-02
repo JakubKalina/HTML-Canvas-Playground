dcdc8e9111921cc8f38a946d04c33472546626ce
Jakub Kalina <kalinakuba6@gmail.com>
2020-05-03 01:45:48 +0200

    Basic URl paths

--- zad2/zad2.js
+++ zad2/zad2.js
@@ -66,7 +66,7 @@ function playGame() {
     // Ustawienie flagi
     backgroundReady = true;
     };
-    backgroundImage.src = "/JakubKalina/HTML-Canvas-Playground/blob/master/zad2/Images/background.jpg";
+    backgroundImage.src = "./Images/background.jpg";
 
 
 
@@ -77,7 +77,7 @@ function playGame() {
     // Ustawienie flagi
     tomReady = true;
     };
-    tomImage.src = "/JakubKalina/HTML-Canvas-Playground/blob/master/zad2/Images/tom.png";
+    tomImage.src = "./Images/tom.png";
 
 
 
@@ -88,7 +88,7 @@ function playGame() {
     // Ustawienie flagi
     jerryReady = true;
     };
-    jerryImage.src = "/JakubKalina/HTML-Canvas-Playground/blob/master/zad2/Images/jerry.png";
+    jerryImage.src = "./Images/jerry.png";
 
     var keysDown = {};
     var tom = {speed: 300};
